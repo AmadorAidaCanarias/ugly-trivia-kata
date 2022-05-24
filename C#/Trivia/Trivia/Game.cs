5f6a7eb47ffdcf96736eb8bcf62a39f584325e25
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-24 15:38:15 +0100

    Roll -> pasar sumas y restas a una asignación compuesta

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -56,7 +56,7 @@ namespace Trivia {
                     _isGettingOutOfPenaltyBox = true;
 
                     Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
-                    _places[_currentPlayer] = _places[_currentPlayer] + roll;
+                    _places[_currentPlayer] += roll;
                     if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
 
                     Console.WriteLine(_players[_currentPlayer]
@@ -71,8 +71,8 @@ namespace Trivia {
                 }
             }
             else {
-                _places[_currentPlayer] = _places[_currentPlayer] + roll;
-                if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
+                _places[_currentPlayer] += roll;
+                if (_places[_currentPlayer] > 11) _places[_currentPlayer] -= 12;
 
                 Console.WriteLine(_players[_currentPlayer]
                         + "'s new location is "
